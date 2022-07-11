421e64b209bcdcde06636dd8fa63e24fddf82d44
unknown <justinchervony@gmail.com>
2022-07-11 16:06:06 -0400

    Create main.py and general project outline

---
+++ main.py
@@ -0,0 +1,26 @@
+destination_list = []
+restaurant_list = []
+transportation_list = []
+entertainment_list = []
+
+print("Welcome to the Day Trip Generator! Let's get started...")
+
+#offer destination option loop
+#if user confirms rng destination, proceed, else remove option from list and randomly generate another
+#if list length < 1, break the program
+
+#offer restaurant option loop
+#if user confirms rng restaurant, proceed, else remove option from list and randomly generate another
+#if list length < 1, break the program
+
+#offer transportation option loop
+#if user confirms rng transportation, proceed, else remove option from list and randomly generate another
+#if list length < 1, break the program
+
+#offer entertainment option loop
+#if user confirms rng entertainment, proceed, else remove option from list and randomly generate another
+#if list length < 1, break the program
+
+#display trip selected options. If confirmed, proceed to next. Else go back to the beginning.
+
+#display full trip. End.
