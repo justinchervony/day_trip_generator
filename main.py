68c9b1bab87d630a317e4396c24735531d3e7b1d
unknown <justinchervony@gmail.com>
2022-07-11 19:00:50 -0400

    Added transportation and restaurant loops. Adjusted destination variables after loop.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 
 destination_list = ["Ireland", "New York", "Miami", "Aspen"]
@@ -26,21 +27,61 @@ while user_trip_confirmation == False :
             break
         elif destination_confirmation_str == "n" and len(destination_list) == 1:
             print("I have no more options to give you. Please restart and try again.")
-            user_trip_confirmation = True
-            break
+            sys.exit()
         elif destination_confirmation_str == "n":
             destination_list.remove(current_destination)
             destination_selected = random.randint(0, len(destination_list) - 1)
             print("Sorry that doesn't work for you. Let me try again...")
         else:
             print("Hmm...that didn't seem like a valid response. Please type 'y' or 'n'.")
-    #offer restaurant option loop
-    #if user confirms rng restaurant, proceed, else remove option from list and randomly generate another
-    #if list length < 1, break the program
 
-    #offer transportation option loop
-    #if user confirms rng transportation, proceed, else remove option from list and randomly generate another
-    #if list length < 1, break the program
+
+    if destination_list[destination_selected] == "Ireland":
+        destination_selected = 0
+    elif destination_list[destination_selected] == "New York":
+        destination_selected = 1
+    elif destination_list[destination_selected] == "Miami":
+        destination_selected = 2
+    else:
+        destination_selected = 3
+    print("Great! Let's continue...")
+
+    user_valid_response = False
+    while user_valid_response == False:
+        transportation_confirmation_str = input(f"For transportation, how about a {transportation_list[transportation_selected]}? y/n ")
+        current_transportation = transportation_list[transportation_selected]
+        if  transportation_confirmation_str == "y":
+            user_valid_response = True
+            break
+        elif transportation_confirmation_str == "n" and len(transportation_list) == 1:
+            print("I have no more options to give you. Please restart and try again.")
+            sys.exit()
+        elif transportation_confirmation_str == "n":
+            transportation_list.remove(current_restaurant)
+            transportation_selected = random.randint(0, len(transportation_list) - 1)
+            print("Sorry that doesn't work for you. Let me try again...")
+        else:
+            print("Hmm...that didn't seem like a valid response. Please type 'y' or 'n'.")
+
+    print("Gotta go fast! Let's continue...")
+
+    user_valid_response = False
+    while user_valid_response == False:
+        restaurant_confirmation_str = input(f"For your dining, how about a {restaurant_list[restaurant_selected]}? y/n ")
+        current_restaurant = restaurant_list[restaurant_selected]
+        if  restaurant_confirmation_str == "y":
+            user_valid_response = True
+            break
+        elif restaurant_confirmation_str == "n" and len(restaurant_list) == 1:
+            print("I have no more options to give you. Please restart and try again.")
+            sys.exit()
+        elif restaurant_confirmation_str == "n":
+            restaurant_list.remove(current_restaurant)
+            restaurant_selected = random.randint(0, len(restaurant_list) - 1)
+            print("Sorry that doesn't work for you. Let me try again...")
+        else:
+            print("Hmm...that didn't seem like a valid response. Please type 'y' or 'n'.")
+
 
     #offer entertainment option loop
     #if user confirms rng entertainment, proceed, else remove option from list and randomly generate another
