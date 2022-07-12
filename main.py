57b6670ffa4222aa72380fdd1fd3a6bb1b988e38
unknown <justinchervony@gmail.com>
2022-07-12 00:12:09 -0400

    Final commit. Project finished and debugged. I need a severe check on how complicated I made this for myself.

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ while user_trip_confirmation == False :
         destination_confirmation_str = input(f"For a destination, how about {destination_list[destination_selected]}? y/n ")
         current_destination = destination_list[destination_selected]
         if  destination_confirmation_str == "y":
+            user_destination_final = current_destination
             user_valid_response = True
             break
         elif destination_confirmation_str == "n" and len(destination_list) == 1:
@@ -53,6 +54,7 @@ while user_trip_confirmation == False :
         transportation_confirmation_str = input(f"For transportation, how about a {transportation_list[transportation_selected]}? y/n ")
         current_transportation = transportation_list[transportation_selected]
         if  transportation_confirmation_str == "y":
+            user_transportation_final = current_transportation
             user_valid_response = True
             break
         elif transportation_confirmation_str == "n" and len(transportation_list) == 1:
@@ -68,12 +70,13 @@ while user_trip_confirmation == False :
     print("Gotta go fast! Let's continue...")
 
     user_valid_response = False
-    restaurant_selected = random.randint(0, len(restaurant_list[destination_selected])) - 1
-    restaurant_list_selected = restaurant_list[restaurant_selected]
+    restaurant_selected = random.randint(0, len(restaurant_list[destination_selected]) -1)
+    restaurant_list_selected = restaurant_list[destination_selected]
     while user_valid_response == False:
         restaurant_confirmation_str = input(f"For your dining, how about a {restaurant_list_selected[restaurant_selected]}? y/n ")
         current_restaurant = restaurant_list_selected[restaurant_selected]
         if  restaurant_confirmation_str == "y":
+            user_restaurant_final = current_restaurant
             user_valid_response = True
             break
         elif restaurant_confirmation_str == "n" and len(restaurant_list_selected) == 1:
@@ -89,31 +92,32 @@ while user_trip_confirmation == False :
     print("Delicious! Let's continue...")
 
     user_valid_response = False
-    entertainment_selected = random.randint(0, len(entertainment_list[destination_selected])) - 1
-    entertainment_list_selected = entertainment_list[entertainment_selected]    
+    entertainment_selected = random.randint(0, len(entertainment_list[destination_selected]) -1)
+    entertainment_list_selected = entertainment_list[destination_selected]    
     while user_valid_response == False:
         entertainment_confirmation_str = input(f"For fun, how would you like to {entertainment_list_selected[entertainment_selected]}? y/n ")
         current_entertainment = entertainment_list_selected[entertainment_selected]
         if  entertainment_confirmation_str == "y":
+            user_entertainment_final = current_entertainment
             user_valid_response = True
             break
-        elif entertainment_confirmation_str == "n" and len(restaurant_list_selected) == 1:
+        elif entertainment_confirmation_str == "n" and len(entertainment_list_selected) == 1:
             print("I have no more options to give you. Please restart and try again.")
             sys.exit()
         elif entertainment_confirmation_str == "n":
-            restaurant_list_selected.remove(current_entertainment)
-            entertainment_selected = random.randint(0, len(restaurant_list_selected) - 1)
+            entertainment_list_selected.remove(current_entertainment)
+            entertainment_selected = random.randint(0, len(entertainment_list_selected) - 1)
             print("Sorry that doesn't work for you. Let me try again...")
         else:
             print("Hmm...that didn't seem like a valid response. Please type 'y' or 'n'.")
 
     print("That sounds like a great way to spend your time!")
     print("")
-    print(f"So your destination is {destination_selected}, your transportation is {transportation_selected}, you picked to eat at a {restaurant_selected}, and you chose to {entertainment_selected}.")
+    print(f"So your destination is {user_destination_final}, your transportation is a {user_transportation_final}, you picked to eat at a {user_restaurant_final}, and you chose to {user_entertainment_final}.")
     user_valid_response = False
    
     while user_valid_response == False:
-        user_finalize_trip = input("Do you want to finalize this trip? y/n")
+        user_finalize_trip = input("Do you want to finalize this trip? y/n ")
         if user_finalize_trip == "y":
             user_valid_response = True
             user_trip_confirmation = True
@@ -124,10 +128,9 @@ while user_trip_confirmation == False :
             transportation_list = ["rental car", "train", "bike"]
             entertainment_list = [["tour the Guinness Factory", "drink an Irishman under the table", "explore an old castle"], ["visit Time Square", "go to the museums", "walk through Central Park"], ["go to the beach", "walk on miracle mile", "hit the clubs"], ["go hiking", "go skiing", "go fishing"]]
             destination_list_length = len(destination_list) - 1
+            break
         else:
             print("Hmm...that didn't seem like a valid response. Please type 'y' or 'n'.")
 
 
-    #display trip selected options. If confirmed, proceed to next. Else go back to the beginning.
-
-#display full trip. End.
+print(f"For your trip, you will take a {user_transportation_final} and go to {user_destination_final}. You'll spend your time eating at {user_restaurant_final} and you will {user_entertainment_final}. Hope you enjoy!")
